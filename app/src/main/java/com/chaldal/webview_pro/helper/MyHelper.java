1a0128d3b1ffb91dddc4565e1d205a239de83c36
anik-biswas <anikbiswas326@gmail.com>
2024-01-28 20:46:24 +0600

    first commit

---
+++ app/src/main/java/com/chaldal/webview_pro/helper/MyHelper.java
@@ -0,0 +1,11 @@
+package com.chaldal.webview_pro.helper;
+
+public interface MyHelper {
+    void loading();
+    void finishLoading();
+    void webGoBack();
+
+    void webLoadUrl(String url);
+    void errorLoading();
+
+} // myHelper End Here ===========
